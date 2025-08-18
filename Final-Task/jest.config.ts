044caa8b684c910e6fc13e4155ddfcb22596a1f0
annah11 <hanamesfin67@gmail.com>
2025-08-18 12:03:58 +0300

    rem

---
+++ Final-Task/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
+
+export default config;
